513401ac5504cda74bf3b090508ae58bbd3a1552
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2025-04-20 18:04:47 +0530

    init

---
+++ xmake.lua
@@ -0,0 +1,25 @@
+add_rules("mode.debug", "mode.release")
+
+set_languages("c++23")
+set_policy("build.sanitizer.address", true)
+set_policy("build.sanitizer.undefined", true)
+add_rules("plugin.compile_commands.autoupdate")
+add_cxxflags("-Wall -Wextra -Wpedantic -Wshadow -g")
+
+local deps = {
+	"glfw",
+	"vulkansdk",
+	"vulkan-headers",
+	"vulkan-loader",
+	"vulkan-hpp",
+	"vulkan-memory-allocator",
+	"glm",
+	"imgui"
+}
+
+add_requires(table.unpack(deps))
+
+target("main")
+set_kind("binary")
+add_files("src/*.cpp")
+add_packages( table.unpack(deps))
