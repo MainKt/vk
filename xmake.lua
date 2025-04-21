1ab0daadc724a38ddfc1a0831692568b38c12a1e
Muhammad Saheed <muhammad.saheed.iam@gmail.com>
2025-04-21 20:27:02 +0530

    update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,10 +1,25 @@
 add_rules("mode.debug", "mode.release")
 
 set_languages("c++23")
-set_policy("build.sanitizer.address", true)
-set_policy("build.sanitizer.undefined", true)
-add_rules("plugin.compile_commands.autoupdate")
-add_cxxflags("-Wall -Wextra -Wpedantic -Wshadow -g")
+
+if is_mode("debug") then
+	set_policy("build.sanitizer.address", true)
+
+	if is_plat("windows") and is_host("windows") then
+		add_cxxflags("/Zi", { tools = { "cl" } })
+	else
+		set_policy("build.sanitizer.undefined", true)
+		add_cxxflags("-g")
+	end
+
+	add_rules("plugin.compile_commands.autoupdate")
+end
+
+if is_plat("windows") and is_host("windows") then
+	add_cxxflags("/W4", { tools = { "cl" } })
+else
+	add_cxxflags("-Wall -Wextra -Wpedantic -Wshadow -Wunused")
+end
 
 add_defines("VK_NO_PROTOTYPES")
 add_defines("GLFW_INCLUDE_VULKAN")
